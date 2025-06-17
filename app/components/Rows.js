d4e5fd4f7ab8784dfa1b690dd46dcb7f1a9bfda1
yernar1121 <pognar1121@gmail.com>
2025-06-17 20:41:59 +0200

    final touches

--- app/components/Rows.js
+++ app/components/Rows.js
@@ -1,13 +1,31 @@
 'use client';
 import { useState, useEffect} from "react"
 
-const Row = ({word, resetKey, setResetKey}) => {
+const Rows = ({setResetKey}) => {
   const [tries, setTries] = useState(()=>Array(5).fill(''));
   const [current, setCurrent] = useState('');
   const [counter, setCounter] = useState(0);
   const [found, setFound] = useState(false);
   const [gameOver, setGameOver]= useState(false);
-  
+  const [word, setWord] = useState('');
+
+  const words = [
+    "enjoy", "dream", "dance", "catch", "carry", "bring", "below", "again", "after", "about",
+    "might", "light", "known", "judge", "inner", "house", "heart", "happy", "front", "first",
+    "think", "sugar", "round", "right", "quick", "place", "party", "night", "movie", "money",
+    "table", "field", "earth", "other", "every", "world", "where", "water", "those", "these",
+    "chart", "voice", "flour", "smile", "green", "black", "laugh", "brown", "white", "young",
+    "apple", "speak", "fruit", "teach", "lemon", "smoke", "sleep", "drink", "spice", "learn",
+    "dairy", "bacon", "candy", "cream", "honey", "nurse", "bread", "pizza", "mango", "river",
+    "dunes", "coast", "frost", "creek", "hills", "plain", "ocean", "beach", "woods", "marsh",
+    "gauge", "stone", "brick", "rotor", "metal", "glass", "steel", "major", "minor", "valve",
+    "chain", "chair", "blade", "panel", "ropes", "press", "lever", "power", "cable", "wheel"
+    ];
+
+  useEffect(()=>{
+    setWord(words[Math.floor(Math.random()*words.length)])
+  },[])
+
   useEffect(()=>{
     if (counter===5 || found==true) return;
 
@@ -67,26 +85,29 @@ const Row = ({word, resetKey, setResetKey}) => {
     }
   },[counter])
 
+  const handleNewGame = () => {
+    setResetKey((c)=>c===0?1:0)
+  }
+
   return (
 
     <div className='flex flex-col gap-2 items-center'>        
         {tries.map((_,i)=>
         <div key={i} className="flex gap-2">
             {Array(5).fill('').map((_,index)=>
-            <div key={index} id={`box-${i}-${index}`} className="h-12 w-12 border-2 flex items-center justify-center">
+            <div key={index} id={`box-${i}-${index}`} className="box h-12 w-12 border-2 flex items-center justify-center">
                 {tries[i][index]?.toUpperCase()}
             </div>)}
         </div>)}
         <p className="text-2xl">
             {found?'You win':counter===5?`You lose: ${word}`:''}
-            
         </p>
         {gameOver?<button className="border-2 py-1 px-2 text-xl border-blue-500 bg-blue-400 cursor-pointer text-white rounded-md"
-        onClick={()=>setResetKey(resetKey===0?1:0)}>Play again?</button>:''}
+        onClick={handleNewGame}>Play again?</button>:''}
     </div>
      
 
   )
 }
 
-export default Row
+export default Rows
--- app/page.js
+++ app/page.js
@@ -5,32 +5,14 @@ import Rows from "./components/Rows";
 import { useState,useEffect } from "react";
 
 export default function Home() {
-  const [word, setWord] = useState('');
-  const [resetKey, setResetKey] = useState(0);
-
-  const words = [
-  "enjoy", "dream", "dance", "catch", "carry", "bring", "below", "again", "after", "about",
-  "might", "light", "known", "judge", "inner", "house", "heart", "happy", "front", "first",
-  "think", "sugar", "round", "right", "quick", "place", "party", "night", "movie", "money",
-  "table", "field", "earth", "other", "every", "world", "where", "water", "those", "these",
-  "chart", "voice", "flour", "smile", "green", "black", "laugh", "brown", "white", "young",
-  "apple", "speak", "fruit", "teach", "lemon", "smoke", "sleep", "drink", "spice", "learn",
-  "dairy", "bacon", "candy", "cream", "honey", "nurse", "bread", "pizza", "mango", "river",
-  "dunes", "coast", "frost", "creek", "hills", "plain", "ocean", "beach", "woods", "marsh",
-  "gauge", "stone", "brick", "rotor", "metal", "glass", "steel", "major", "minor", "valve",
-  "chain", "chair", "blade", "panel", "ropes", "press", "lever", "power", "cable", "wheel"
-];
-
   
-  useEffect(()=>{
-    setWord(words[Math.floor(Math.random()*words.length)])
-  },[resetKey])
+  const [resetKey, setResetKey] = useState(0);
 
   return (
     <div className="flex items-center justify-center flex-col gap-3 h-[100vh]">
       <h1 className="text-4xl font-bold">YernaRDLE</h1>
       <p className="text-2xl">Guess the 5 letter word</p>
-      <Rows word={word} setWord={setWord} key={resetKey} resetKey={resetKey} setResetKey={setResetKey}/>
+      <Rows key={resetKey} setResetKey={setResetKey}/>
     </div>
   );
 }
